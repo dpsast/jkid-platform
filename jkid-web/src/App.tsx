3724b29db0998c16878686260d3a52838d474184
Seijo Cecilia <willamwei@outlook.com>
2024-06-19 15:26:26 +0800

    feat(web): landing page

--- jkid-web/src/App.tsx
+++ jkid-web/src/App.tsx
@@ -1,4 +1,15 @@
-import {AppBar, Box, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography} from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  createTheme,
+  CssBaseline,
+  Grid,
+  Link,
+  ThemeProvider,
+  Toolbar,
+  Typography
+} from "@mui/material";
 import {Outlet} from "react-router-dom";
 
 const theme = createTheme({
@@ -43,4 +54,25 @@ function App() {
   </ThemeProvider>
 }
 
+function Landing() {
+  return <Box height={"100%"} display={"flex"} alignItems={"center"}><Grid container sx={{flexGrow: 1}}>
+    <Grid xs={1} md={3}/>
+    <Grid xs={10} md={6} sx={{display: "flex", flexDirection: "column", gap: 2}}>
+      <Box textAlign={"center"}>
+        <Typography variant={"h4"}>饥渴 ID 平台</Typography>
+      </Box>
+      <Typography>若要访问物理系学生科协所维护的资源（如<Link href={"https://wiki.jkparadise.space/"}>维饥百渴</Link>），请先注册饥渴 ID。</Typography>
+      <Typography>原则上，我们只允许清华大学物理系与致理书院物理方向的同学注册。其他院系的同学如果有注册意愿，请通过邮件单独联系我们。</Typography>
+      <Typography>清华大学在校学生请点击下面的“在校学生注册”进入注册流程。我们将会先将你重定向到 Tsinghua Git，请在那里先登录，登录完毕后，你将回到注册页面。我们需要获取你的<b>真实姓名、学号、学生邮箱</b>以便审核你的身份。除此之外，不会读取任何其他信息。</Typography>
+      <Typography>如果你不再拥有清华大学电子身份（例如，已经从清华大学毕业），不用担心！请点击<Link>其他人员注册</Link>以进入注册页面。</Typography>
+      <Button size={"large"} variant={"contained"} sx={{ alignSelf: "center" }} href={
+        new URL("/register/fire", import.meta.env.VITE_BACKEND_ADDRESS).toString()
+      }>在校学生注册</Button>
+
+    </Grid>
+    <Grid xs={1} md={3}/>
+  </Grid></Box>
+}
+
 export default App;
+export { Landing };
