e871d746db393480444732dfb13bf426b147bf49
zeauw <me@zeauw.dev>
2025-02-19 01:43:13 +0800

    chore: update link

--- jkid-web/src/Register.tsx
+++ jkid-web/src/Register.tsx
@@ -85,7 +85,7 @@ function SubmittedPending() {
     <Grid xs={1} md={3}/>
     <Grid xs={10} md={6}>
       <Typography>感谢你的注册！请等待我们审核。</Typography>
-      <Typography>审核通过后，你的学生邮箱将会收到一封邮件。你可以在一段时间后尝试用注册时的用户名和临时密码登录我们的 <Link href={"https://gitea.jkparadise.space/"}>Gitea 平台</Link>。</Typography>
+      <Typography>审核通过后，你的学生邮箱将会收到一封邮件。你可以在一段时间后尝试用注册时的用户名和临时密码登录我们的 <Link href={"https://git.dpsast.org/"}>Gitea 平台</Link>。</Typography>
     </Grid>
     <Grid xs={1} md={3}/>
   </Grid></Box>;
@@ -103,7 +103,7 @@ function AutoPass() {
       <Typography>感谢你的注册！你的学号位于我们维护的“自动通过名单”中，因此恭喜你已经拥有了饥渴 ID！</Typography>
       <Typography>你的用户名：<b>{username}</b>；你的临时密码：<b>{tempPassword}</b></Typography>
       <Typography>请注意这是一个<b>临时</b>密码，在审核通过第一次登录时依然会要求你修改密码。</Typography>
-      <Typography>欢迎用以上凭据登录我们的 <Link href={"https://gitea.jkparadise.space/"}>Gitea 平台</Link>！</Typography>
+      <Typography>欢迎用以上凭据登录我们的 <Link href={"https://git.dpsast.org/"}>Gitea 平台</Link>！</Typography>
     </Grid>
     <Grid xs={1} md={3}/>
   </Grid></Box>;
